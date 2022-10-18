f947f9075d828541355d5871b6edb6bea8ac1843
Uladzimir <blind72@mail.ru>
2022-10-18 18:59:41 +0200

    Example 2 added

---
+++ Example002_Massiv/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти максимальное из 9 чисел через массив
+
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {11, 20, 13, 24, 75, 56, 70, 18, 59};
+
+//Если мы хотим, чтобы нам вывело 1 число из массива, то можно:
+//Console.WriteLine(array[5]);
+
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
+
+
